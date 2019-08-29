6db852e9f99de4e03f7a73b4c57635f5ee3e15af
Jeremy Ravet <gamemaker6.1@gmail.com>
2019-08-29 11:53:04 -0700

    Added Michael's picture to his leadership section

--- resources/views/tatham/our_story.blade.php
+++ resources/views/tatham/our_story.blade.php
@@ -9,7 +9,12 @@
                 <div class="ml-5 mr-5"> {{ __('tatham.our_story_content.how_bootcamp_works_content') }} </div>
         <div class="justify-content-center d-flex huge"> {{ __('tatham.our_story_content.leadership_mega_header') }} </div>
             <h3> {{ __('tatham.our_story_content.michael_tatham_header') }} </h3>
-                <div class="ml-5 mr-5"> {{ __('tatham.our_story_content.michael_tatham_content') }} </div>
+                <div class="ml-5 mr-5">
+                    <div class="float-right m-2">
+                        <img src="{{ asset('images/pres2.jpg')}}">
+                    </div>
+                    {{ __('tatham.our_story_content.michael_tatham_content') }}
+                </div>
             <h3> {{ __('tatham.our_story_content.laurie_clarke_header') }} </h3>
                 <div class="ml-5 mr-5"> {{ __('tatham.our_story_content.laurie_clarke_content') }} </div>
         <div class="justify-content-center d-flex huge"> {{ __('tatham.our_story_content.client_results_mega_header') }} </div>
