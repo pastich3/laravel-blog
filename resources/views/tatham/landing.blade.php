22de08d9238ed0564419c140971f4bca2b81a3a2
Jeremy Ravet <gamemaker6.1@gmail.com>
2019-08-29 17:55:05 -0700

    Minor styling changes on main page

--- resources/views/tatham/landing.blade.php
+++ resources/views/tatham/landing.blade.php
@@ -4,16 +4,16 @@
 	<div>
 		<div class="">
 			<div>
-				<div class="font-weight-bold">
+				<div class="font-weight-bold d-flex justify-content-center">
 					{{ __('tatham.landing_page_question_header') }}
 				</div>
-				<div class="mt-1">
+				<div class="mt-1 mx-3">
 					{{ __('tatham.landing_page_question_content') }}
 				</div>
 			</div>
 		</div>
 	</div>
-	<div>
+	<div class="mt-2">
 		@include('tatham._videos_embed')
 	</div>
 	<!-- <div style="background-color: lightgray; height: 430px" class="mt-3 d-flex justify-content-center align-items-center">
