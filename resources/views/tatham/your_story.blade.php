17aed5dbc3f3ad9a14280c49f41020d87096a5b6
Jeremy Ravet <gamemaker6.1@gmail.com>
2019-08-30 19:00:35 -0700

    Testing a style change on your-story page

--- resources/views/tatham/your_story.blade.php
+++ resources/views/tatham/your_story.blade.php
@@ -1,27 +1,50 @@
 @extends('layouts.app')
 @section('content')
 	<div class="justify-content-center d-flex font-weight-bolder huge"> {{ __('tatham.your_story') }} </div>
-		<div class="justify-content-center d-flex huge"> {{ __('tatham.your_story_content.inaction_mega_header') }} </div>
-			<h3> {{ __('tatham.your_story_content.cost_of_inaction_header') }} </h3>
-				<div class="ml-5 mr-5"> {!! __('tatham.your_story_content.cost_of_inaction_content') !!} </div>
-			<h3> {{ __('tatham.your_story_content.difficulty_accomplishing_change_header') }} </h3>
-				<div class="ml-5 mr-5"> {!! __('tatham.your_story_content.difficulty_accomplishing_change_content') !!} </div>
-		<div class="justify-content-center d-flex huge"> {{ __('tatham.your_story_content.change_mega_header') }} </div>
-			<h3> {{ __('tatham.your_story_content.change_header') }} </h3>
-				<div class="ml-5 mr-5"> {!! __('tatham.your_story_content.change_content') !!} </div>
-		<div class="justify-content-center d-flex huge"> {{ __('tatham.your_story_content.journey_mega_header') }} </div>
-			<h3> {{ __('tatham.your_story_content.start_journey_header') }} </h3>
-				<div class="ml-5 mr-5"> {!! __('tatham.your_story_content.start_journey_content') !!} </div>
-			<h3> {{ __('tatham.your_story_content.boot_camp_header') }} </h3>
-				<div class="ml-5 mr-5"> {!! __('tatham.your_story_content.boot_camp_content') !!} </div>
-			<h3> {{ __('tatham.your_story_content.sustainability_header') }} </h3>
-				<div class="ml-5 mr-5"> {!! __('tatham.your_story_content.sustainability_content') !!} </div>
-		<div class="justify-content-center d-flex huge"> {{ __('tatham.your_story_content.results_mega_header') }} </div>
-			<h3> {{ __('tatham.your_story_content.can_you_header') }} </h3>
-				<div class="ml-5 mr-5"> {!! __('tatham.your_story_content.can_you_content') !!} </div>
-			<h3> {{ __('tatham.your_story_content.clients_header') }} </h3>
-				<div class="ml-5 mr-5"> {!! __('tatham.your_story_content.clients_content') !!} </div>
-
+		<div style="border: 1px solid black; background-color: #D1D1D1" class="px-3 pb-3">
+			<span class="justify-content-center d-flex huge"> {{ __('tatham.your_story_content.inaction_mega_header') }} </span>
+			<div style="border: 1px solid gray; background-color: #C1C1C1" class="p-3">
+				<div class="h3"> {{ __('tatham.your_story_content.cost_of_inaction_header') }} </div>
+				<div class="ml-3 mr-3"> {!! __('tatham.your_story_content.cost_of_inaction_content') !!} </div>
+			</div>
+			<div style="border: 1px solid gray; background-color: #C1C1C1" class="mt-3 p-3">
+				<div class="h3"> {{ __('tatham.your_story_content.difficulty_accomplishing_change_header') }} </div>
+				<div class="ml-3 mr-3"> {!! __('tatham.your_story_content.difficulty_accomplishing_change_content') !!} </div>
+			</div>
+		</div>
+		<div style="border: 1px solid black; background-color: #D1D1D1" class="px-3 pb-3">
+			<span class="justify-content-center d-flex huge"> {{ __('tatham.your_story_content.change_mega_header') }} </span>
+			<div style="border: 1px solid gray; background-color: #C1C1C1" class="p-3">
+				<div class="h3"> {{ __('tatham.your_story_content.change_header') }} </div>
+				<div class="ml-3 mr-3"> {!! __('tatham.your_story_content.change_content') !!} </div>
+			</div>
+		</div>
+		<div style="border: 1px solid black; background-color: #D1D1D1" class="px-3 pb-3">
+			<span class="justify-content-center d-flex huge"> {{ __('tatham.your_story_content.journey_mega_header') }} </span>
+			<div style="border: 1px solid gray; background-color: #C1C1C1" class="p-3">
+				<div class="h3"> {{ __('tatham.your_story_content.start_journey_header') }} </div>
+				<div class="ml-3 mr-3"> {!! __('tatham.your_story_content.start_journey_content') !!} </div>
+			</div>
+			<div style="border: 1px solid gray; background-color: #C1C1C1" class="mt-3 p-3">
+				<div class="h3"> {{ __('tatham.your_story_content.boot_camp_header') }} </div>
+				<div class="ml-3 mr-3"> {!! __('tatham.your_story_content.boot_camp_content') !!} </div>
+			</div>
+			<div style="border: 1px solid gray; background-color: #C1C1C1" class="mt-3 p-3">
+				<div class="h3"> {{ __('tatham.your_story_content.sustainability_header') }} </div>
+				<div class="ml-3 mr-3"> {!! __('tatham.your_story_content.sustainability_content') !!} </div>
+			</div>
+		</div>
+		<div style="border: 1px solid black; background-color: #D1D1D1" class="px-3 pb-3">
+			<span class="justify-content-center d-flex huge"> {{ __('tatham.your_story_content.results_mega_header') }} </span>
+			<div style="border: 1px solid gray; background-color: #C1C1C1" class="p-3">
+				<div class="h3"> {{ __('tatham.your_story_content.can_you_header') }} </div>
+				<div class="ml-3 mr-3"> {!! __('tatham.your_story_content.can_you_content') !!} </div>
+			</div>
+			<div style="border: 1px solid gray; background-color: #C1C1C1" class="mt-3 p-3">
+				<div class="h3"> {{ __('tatham.your_story_content.clients_header') }} </div>
+				<div class="ml-3 mr-3"> {!! __('tatham.your_story_content.clients_content') !!} </div>
+			</div>
+		</div>
 	@include('tatham._testimonial_carousel')
 
 @endsection
