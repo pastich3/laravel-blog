207d46e944e5d5bb32297b93eac0934bd2626689
Jeremy Ravet <gamemaker6.1@gmail.com>
2019-09-05 14:12:07 -0700

    Added a little styling to the cookie-consent dialog

--- resources/views/vendor/cookieConsent/dialogContents.blade.php
+++ resources/views/vendor/cookieConsent/dialogContents.blade.php
@@ -1,11 +1,12 @@
-<div class="js-cookie-consent cookie-consent">
+<div class="js-cookie-consent cookie-consent w-100 py-2"
+    style="border-bottom: 1px double lightgrey">
+    <div class="d-flex justify-content-center align-items-center">
+        <span class="cookie-consent__message">
+            {!! trans('cookieConsent::texts.message') !!}
+        </span>
 
-    <span class="cookie-consent__message">
-        {!! trans('cookieConsent::texts.message') !!}
-    </span>
-
-    <button class="js-cookie-consent-agree cookie-consent__agree">
-        {{ trans('cookieConsent::texts.agree') }}
-    </button>
-
+        <button class="js-cookie-consent-agree cookie-consent__agree ml-2 btn btn-info">
+            {{ trans('cookieConsent::texts.agree') }}
+        </button>
+    </div>
 </div>
