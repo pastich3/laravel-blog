01d537f054b21f05254610603e22f86836858e81
Jeremy Ravet <gamemaker6.1@gmail.com>
2019-09-09 16:06:31 -0700

    Overlayed text on top of the Toronto/Portland images

--- resources/views/tatham/landing.blade.php
+++ resources/views/tatham/landing.blade.php
@@ -474,8 +474,14 @@
       <!-- Uncomment below if you wan to use dynamic maps -->
       <!-- <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2885.998348021916!2d-79.40549138406612!3d43.66900417912078!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882b3497e73fff69%3A0xecf090b8db1452c2!2s43%20Madison%20Ave%2C%20Toronto%2C%20ON%20M5R%202S2%2C%20Canada!5e0!3m2!1sen!2sus!4v1568064190263!5m2!1sen!2sus" width="100%" height="380" frameborder="0" style="border:0" allowfullscreen></iframe> -->
         <div class="d-flex">
-          <img src="/images/toronto.jpeg" style="width:50%" alt="picture of Toronto skyline">
-          <img src="/images/portland.jpeg" style="width:50%"alt="picture of Portland skyline">
+          <div class="position-relative w-50 d-flex flex-column justify-content-center align-items-center">
+            <img class="w-100" src="/images/toronto.jpeg" alt="picture of Toronto skyline">
+            <div class="position-absolute" style="color: white; font-size: 2.5em; font-weight: bold; left: 50%; top: 50%; transform: translate(-50%, -50%);"> Toronto </div>
+          </div>
+          <div class="position-relative w-50 d-flex flex-column justify-content-center align-items-center">
+            <img class="w-100" src="/images/portland.jpeg" alt="picture of Portland skyline">
+            <div class="position-absolute" style="color: white; font-size: 2.5em; font-weight: bold; left: 50%; top: 50%; transform: translate(-50%, -50%);"> Portland </div>
+          </div>
         </div>
       <div class="container wow fadeInUp mt-5">
         <div class="row justify-content-center">
