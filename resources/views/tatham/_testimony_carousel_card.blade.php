13dd9a7b7daf657f170777064540b29dc214c315
Jeremy Ravet <gamemaker6.1@gmail.com>
2019-09-09 15:40:48 -0700

    Increased padding of testimony-cards to give the arrows some breathing room

--- resources/views/tatham/_testimony_carousel_card.blade.php
+++ resources/views/tatham/_testimony_carousel_card.blade.php
@@ -1,11 +1,11 @@
-<div class="py-3 px-5 carousel-item {{$itemActive ?? ''}}">
+<div style="padding-left: 4em; padding-right: 4em" class="py-3 carousel-item {{$itemActive ?? ''}}">
 	<!-- <div class="px-5 d-flex flex-fill flex-column"> -->
 		<div class="d-flex justify-content-center align-items-center" style="margin-bottom: 20px">
 			<span style="max-width: 800px">
 			{!! $content !!}
 			</span>
 		</div>
-		<div style="position: absolute; bottom: 0.75em; right: 0.75em;" class="d-flex align-items-end justify-content-end flex-column">
+		<div style="position: absolute; bottom: 0.75em; right: 4.75em;" class="d-flex align-items-end justify-content-end flex-column">
 			<div style="font-size: 0.65em;" class="d-flex font-italic justify-content-end">
 				{{ $person }}, {{ $title }}
 			</div>
