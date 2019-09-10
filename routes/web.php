df1d03b1392dc009203bfa01e91029b8269a6b82
Jeremy Ravet <gamemaker6.1@gmail.com>
2019-09-10 16:07:42 -0700

    Commented out currently unused web/api routes

--- routes/web.php
+++ routes/web.php
@@ -12,16 +12,16 @@
 */
 
 Route::get('/', 'HomeController@index')->name('home');
-Route::get('/case_studies', 'HomeController@case_studies')->name('case_studies');
-Route::get('/your_story', 'HomeController@yourStory')->name('your_story');
-Route::get('/our_story', 'HomeController@ourStory')->name('our_story');
-Route::get('/contact', 'HomeController@contact')->name('contact');
-Route::get('/contact_submitted', 'HomeController@contactInfoSubmitted')->name('contact_submitted');
-Route::get('/testimonials', 'HomeController@testimonials')->name('testimonials');
+// Route::get('/case_studies', 'HomeController@case_studies')->name('case_studies');
+// Route::get('/your_story', 'HomeController@yourStory')->name('your_story');
+// Route::get('/our_story', 'HomeController@ourStory')->name('our_story');
+// Route::get('/contact', 'HomeController@contact')->name('contact');
+// Route::get('/contact_submitted', 'HomeController@contactInfoSubmitted')->name('contact_submitted');
+// Route::get('/testimonials', 'HomeController@testimonials')->name('testimonials');
 Route::post('/contact', 'TathamContactController@contact')->name('contactPost');
-Route::get('/blog', 'PostController@index')->name('blog');
-Route::get('/posts/feed', 'PostFeedController@index')->name('posts.feed');
-Route::resource('posts', 'PostController')->only('show');
-Route::resource('users', 'UserController')->only('show');
+// Route::get('/blog', 'PostController@index')->name('blog');
+// Route::get('/posts/feed', 'PostFeedController@index')->name('posts.feed');
+// Route::resource('posts', 'PostController')->only('show');
+// Route::resource('users', 'UserController')->only('show');
 
-Route::get('newsletter-subscriptions/unsubscribe', 'NewsletterSubscriptionController@unsubscribe')->name('newsletter-subscriptions.unsubscribe');
+// Route::get('newsletter-subscriptions/unsubscribe', 'NewsletterSubscriptionController@unsubscribe')->name('newsletter-subscriptions.unsubscribe');
