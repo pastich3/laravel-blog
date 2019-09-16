6d0f5525aa6a2a88940d631d27502d1a2d1fb0a4
Jeremy Ravet <gamemaker6.1@gmail.com>
2019-09-16 15:07:13 -0700

    WIP playing with truths

--- resources/views/tatham/landing_our_approach.blade.php
+++ resources/views/tatham/landing_our_approach.blade.php
@@ -68,13 +68,191 @@
               </div>
             </div>
           </div>
-
-        </div>
-        <div class="content">
-
         </div>
       </div>
     </section><!-- #facts -->
+    <section id="about">
+      <div class="container">
+        <div class="row about-container">
+
+          <div class="col-lg-6 content order-lg-1 order-2">
+            <h2 class="title">30 years of success</h2>
+            <p>
+For more than 30 years, a blue-chip roster of clients
+has quietly, almost secretely, turned to Tatham.
+Each had its own distinctive problem. Yet each faced a
+common situation: how to get the results
+targeted by their own strategies, quickly.
+            </p>
+
+            <!-- <div class="icon-box wow fadeInUp">
+              <div class="icon"><i class="fa fa-shopping-bag"></i></div>
+              <h4 class="title"><a href="">Eiusmod Tempor</a></h4>
+              <p class="description">Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi</p>
+            </div>
+
+            <div class="icon-box wow fadeInUp" data-wow-delay="0.2s">
+              <div class="icon"><i class="fa fa-photo"></i></div>
+              <h4 class="title"><a href="">Magni Dolores</a></h4>
+              <p class="description">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+            </div>
+
+            <div class="icon-box wow fadeInUp" data-wow-delay="0.4s">
+              <div class="icon"><i class="fa fa-bar-chart"></i></div>
+              <h4 class="title"><a href="">Dolor Sitema</a></h4>
+              <p class="description">Minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat tarad limino ata</p>
+            </div> -->
+
+          </div>
+
+          <div class="col-lg-6 background order-lg-2 order-1 wow fadeInRight"></div>
+        </div>
+
+      </div>
+    </section><!-- #about -->
+    <section id="facts">
+      <div class="container wow fadeIn">
+        <div class="section-header">
+          <!-- <h3 class="section-title">What we offer</h3> -->
+          <!-- <p class="section-description">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque</p> -->
+
+        </div>
+        <div class="content d-flex justify-content-center w-100">
+          <div class="px-3" style="max-width: 750px;">
+            <p>The essence of what we offer is powerful. With the Tatham approach,
+            you build an organization capability that can execute strategy and
+            get measurable results faster than you thought possible. Plus there’s
+            an important side effect: you break the cycle of hell where recurring
+            business problems must constantly be solved. We provide a systematic
+            process that allows your people to solve problems, once.</p>
+            <p>Over the years, we believe there’s no competitive offering that’s
+            comparable to ours or has the unbroken, 30-year history of successes
+            that we’ve achieved with our clients.</p>
+            <p>At Tatham, our approach connects pieces of your strategy puzzle into an
+            unanticipated source of competitive advantage: vastly improved business
+            processes. Your own people, not us, using tribal knowledge culled from
+            the front line of your business, end up delivering results. We simply
+            provide your people with the capability, tools and insight to execute your
+            business strategy. You begin to see bottom-line results, usually within 90
+            days, as your organization’s operating culture transforms itself.</p>
+            <p>The Tatham Group has had the distinct privilege of working in the
+            background with CEOs at some major companies. Bank of America,
+            Rockwell International, IBM Canada, TRW, Hughes Space and Electronic
+            Systems, among others. Before retaining us, each sought to improve in
+            its own way some aspect of its business operations. Some tried internal
+            task forces. Others turned to outside consultants. Many grew frustrated
+            with good intentions gone astray. Why? The bottom-line results each
+            sought never materialized or could never be sustained</p>
+
+            <p>Working with some of the smartest companies around has taught us
+            some important truths about the way we deliver value to them. A few of
+            those stark truths are revealed on these pages for you to consider, to
+            challenge, and to engage us in a dialogue.</p>
+            <p>We encourage you to do so.</p>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- #facts -->
+     <section id="clients">
+      <div class="container wow fadeIn">
+        <div class="section-header" style="margin-bottom: 80px;">
+          <h3 class="section-title">Truths</h3>
+          <!-- <p class="section-description">Here are some of our clients</p> -->
+        </div>
+        <div class="d-flex justify-content-center">
+          <div class="row" style="max-width: 1000px;" >
+            <div class="wow fadeInUp w-100" data-wow-delay="0.2s">
+              <div class="box box-shadow">
+                <div class="d-flex justify-content-start" style="">
+                  <div class="truth"> &gt; TRUTH #<span class="number">1</span></div>
+                </div>
+                <h4 class="title d-flex justify-content-start">YOU DEVELOP A STRATEGY THAT BOGS DOWN WHEN IMPLEMENTED.</h4>
+
+                <div class="d-flex justify-content-end">
+                  <div style="max-width: 450px; text-align: left;">
+                    <span class="mb-2 d-flex justify-content-start"> WE ENABLE COMPANIES TO ACHIEVE SUPERIOR PERFORMANCE THROUGH MASTERFUL EXECUTION OF THEIR STRATEGY.</span>
+                    <span class="mb-2 d-flex justify-content-start text-tatham-teal"> Our Approach: </span>
+                    <ul>
+                    <li>Create a culture that thrives on a revitalized, company-wide customer orientation.</li>
+                    <li>Focus on performance constraints that prevent you from delivering on your commitments.</li>
+                    <li>Empower the front-line people to innovate and implement solutions. </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+
+            <div class="wow fadeInUp w-100" data-wow-delay="0.2s">
+              <div class="box box-shadow">
+                <div class="d-flex justify-content-start">
+                  <div class="truth"> &gt; TRUTH #<span class="number">2</span></div>
+                </div>
+                <h4 class="title d-flex justify-content-start">YOU DEVELOP A STRATEGY THAT BOGS DOWN WHEN IMPLEMENTED.</h4>
+
+                <div class="d-flex justify-content-end">
+                  <div style="max-width: 450px; text-align: left;">
+                    <span class="mb-2 d-flex justify-content-start"> WE ENABLE COMPANIES TO ACHIEVE SUPERIOR PERFORMANCE THROUGH MASTERFUL EXECUTION OF THEIR STRATEGY.</span>
+                    <span class="mb-2 d-flex justify-content-start text-tatham-teal"> Our Approach: </span>
+                    <ul>
+                    <li>Create a culture that thrives on a revitalized, company-wide customer orientation.</li>
+                    <li>Focus on performance constraints that prevent you from delivering on your commitments.</li>
+                    <li>Empower the front-line people to innovate and implement solutions. </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+
+
+            <div class="wow fadeInUp w-100" data-wow-delay="0.2s">
+              <div class="box box-shadow">
+                <div class="d-flex justify-content-start">
+                  <div class="truth"> &gt; TRUTH #<span class="number">3</span></div>
+                </div>
+                <h4 class="title d-flex justify-content-start">YOU DEVELOP A STRATEGY THAT BOGS DOWN WHEN IMPLEMENTED.</h4>
+
+                <div class="d-flex justify-content-end">
+                  <div style="max-width: 450px; text-align: left;">
+                    <span class="mb-2 d-flex justify-content-start"> WE ENABLE COMPANIES TO ACHIEVE SUPERIOR PERFORMANCE THROUGH MASTERFUL EXECUTION OF THEIR STRATEGY.</span>
+                    <span class="mb-2 d-flex justify-content-start" style="color: green;"> Our Approach: </span>
+                    <ul>
+                    <li>Create a culture that thrives on a revitalized, company-wide customer orientation.</li>
+                    <li>Focus on performance constraints that prevent you from delivering on your commitments.</li>
+                    <li>Empower the front-line people to innovate and implement solutions. </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!-- <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.2s">
+              <div class="box">
+                <div class="icon"><a href=""><i class="fa fa-photo"></i></a></div>
+                <h4 class="title"><a href="">Magni Dolores</a></h4>
+                <p class="description">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.4s">
+              <div class="box">
+                <div class="icon"><a href=""><i class="fa fa-road"></i></a></div>
+                <h4 class="title"><a href="">Nemo Enim</a></h4>
+                <p class="description">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque</p>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.6s">
+              <div class="box">
+                <div class="icon"><a href=""><i class="fa fa-shopping-bag"></i></a></div>
+                <h4 class="title"><a href="">Eiusmod Tempor</a></h4>
+                <p class="description">Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi</p>
+              </div>
+            </div> -->
+          </div>
+        </div>
+
+      </div>
+    </section><!-- #clients -->
   </main>
   <!--==========================
     Footer
