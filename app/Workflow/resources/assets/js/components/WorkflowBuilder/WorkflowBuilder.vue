f873e1bc287bbb3c107dd21f3de8bb48dfa700f6
Jeremy Ravet <gamemaker6.1@gmail.com>
2020-03-18 11:30:03 -0700

    Scaffolding Vue components

---
+++ app/Workflow/resources/assets/js/components/WorkflowBuilder/WorkflowBuilder.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="d-flex justify-content-between">
+        <div class="d-flex justify-content-center">
+            <workflow-component-list></workflow-component-list>
+        </div>
+        <div class="d-flex justify-content-center">
+            <workflow-builder-canvas></workflow-builder-canvas>
+        </div>
+    </div>
+</template>
+<script>
+    import WorkflowBuilderCanvas from "./WorkflowBuilderCanvas";
+    import WorkflowComponentList from "./WorkflowComponentList";
+    import WorkflowApiRequests from "../WorkflowApiRequests";
+    export default {
+        components: {
+            "workflow-builder-canvas": WorkflowBuilderCanvas,
+            "workflow-component-list": WorkflowComponentList
+        }
+    }
+</script>
+<style scoped lang="scss">
+
+</style>
--- app/Workflow/resources/assets/js/components/WorkflowBuilder/WorkflowComponentList.vue
+++ app/Workflow/resources/assets/js/components/WorkflowBuilder/WorkflowComponentList.vue
@@ -3,6 +3,9 @@
     </div>
 </template>
 <script>
+    export default {
+
+    }
 </script>
 <style scoped lang="scss">
 
