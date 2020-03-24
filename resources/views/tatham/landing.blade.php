136fb156c7ddf48c5fe5fe80ebc5cb33453863b1
Jeremy Ravet <gamemaker6.1@gmail.com>
2020-03-24 12:03:29 -0700

    Refactored landing and consult pages

--- resources/views/tatham/landing.blade.php
+++ resources/views/tatham/landing.blade.php
@@ -1,85 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-  <title>Tatham</title>
-  <meta content="width=device-width, initial-scale=1.0" name="viewport">
-  <meta content="" name="keywords">
-  <meta content="" name="description">
-
-  <!-- Favicons -->
-  <link href="images/newfavicon.png" rel="icon">
-  <link href="img/apple-touch-icon.png" rel="apple-touch-icon">
-
-  <!-- Google Fonts -->
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Poppins:300,400,500,700|Lato|Montserrat" rel="stylesheet">
-  <!-- Bootstrap CSS File -->
-  <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-  <!-- Libraries CSS Files -->
-  <link href="lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
-  <link href="lib/animate/animate.min.css" rel="stylesheet">
-
-  <!-- Main Stylesheet File -->
-  <link href="css/style.css" rel="stylesheet">
-
-  <!-- =======================================================
-    Theme Name: Regna
-    Theme URL: https://bootstrapmade.com/regna-bootstrap-onepage-template/
-    Author: BootstrapMade.com
-    License: https://bootstrapmade.com/license/
-  ======================================================= -->
-</head>
-
-<body>
-
-  <!--==========================
-  Header
-  ============================-->
-  <header id="header">
-    <div class="container">
-
-      <div id="logo" class="pull-left" style="margin-top:-14px">
-        <a href="#home" class="no-hover-highlight">
-        <!-- <img src="images/newfavicon.png" class="" style="margin-top:-16px; height: 50px" alt="" title="" /> -->
-        <span style="font-family: 'Montserrat'; letter-spacing:1px; font-size: 2em; font-style: regular">&nbsp;Tatham Inc</span>
-        </a>
-        <!-- Uncomment below if you prefer to use a text logo -->
-        <!--<h1><a href="#home">Regna</a></h1>-->
-      </div>
-
-      <nav id="nav-menu-container">
-        <ul class="nav-menu">
-          <li class="menu-active"><a href="#home">Home</a></li>
-          <li><a href="#about">About Us</a></li>
-          <li><a href="#bootcamp">Boot Camp</a></li>
-          <li><a href="#clients">Clients</a></li>
-          <!-- <li><a href="#bootcamp">Bootcamp</a></li> -->
-          <!-- <li><a href="#portfolio">Case Studies</a></li> -->
-          <!-- <li><a href="#team">Team</a></li> -->
-          <!-- <li class="menu-has-children"><a href="">Drop Down</a>
-            <ul>
-              <li><a href="#">Drop Down 1</a></li>
-              <li class="menu-has-children"><a href="#">Drop Down 2</a>
-                <ul>
-                  <li><a href="#">Deep Drop Down 1</a></li>
-                  <li><a href="#">Deep Drop Down 2</a></li>
-                  <li><a href="#">Deep Drop Down 3</a></li>
-                  <li><a href="#">Deep Drop Down 4</a></li>
-                  <li><a href="#">Deep Drop Down 5</a></li>
-                </ul>
-              </li>
-              <li><a href="#">Drop Down 3</a></li>
-              <li><a href="#">Drop Down 4</a></li>
-              <li><a href="#">Drop Down 5</a></li>
-            </ul>
-          </li> -->
-          <li><a href="#contact">Contact Us</a></li>
-        </ul>
-      </nav><!-- #nav-menu-container -->
-    </div>
-  </header><!-- #header -->
-
+@extends('layouts.landing')
+@section('content')
   <!--==========================
     Hero Section
   ============================-->
@@ -578,43 +498,8 @@ starting with your customers.</p>
     @include('tatham._landing_contact')
 
   </main>
-
-  <!--==========================
-    Footer
-  ============================-->
-  <footer id="footer">
-    <div class="footer-top">
-      <div class="container">
-
-      </div>
-    </div>
-
-    <div class="container">
-      <div class="copyright">
-        &copy; Copyright <strong>Tatham Inc</strong>. All Rights Reserved
-      </div>
-
-    </div>
-  </footer><!-- #footer -->
-
-  <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
-
-  <!-- JavaScript Libraries -->
-  <script src="lib/jquery/jquery.min.js"></script>
-  <script src="lib/jquery/jquery-migrate.min.js"></script>
-  <script src="lib/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="lib/easing/easing.min.js"></script>
-  <script src="lib/wow/wow.min.js"></script>
-  <script src="lib/waypoints/waypoints.min.js"></script>
-  <script src="lib/counterup/counterup.min.js"></script>
-  <script src="lib/superfish/hoverIntent.js"></script>
-  <script src="lib/superfish/superfish.min.js"></script>
-
-  <!-- Contact Form JavaScript File -->
-  <script src="contactform/contactform.js"></script>
-
-  <!-- Template Main Javascript File -->
-  <script src="js/main.js"></script>
+@endsection
+@push('inline-scripts')
 
   <script id="starsAnimationScript">
     $(document).ready(function(){
@@ -711,7 +596,4 @@ starting with your customers.</p>
       setTimeout(function() {par.insertBefore(el, next);}, 0)
     }
   </script>
-  @stack('inline-scripts')
-
-</body>
-</html>
+@endpush
--- resources/views/tatham/landing_consult.blade.php
+++ resources/views/tatham/landing_consult.blade.php
@@ -1,61 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-  <title>Tatham</title>
-  <meta content="width=device-width, initial-scale=1.0" name="viewport">
-  <meta content="" name="keywords">
-  <meta content="" name="description">
+@extends('layouts.landing')
 
-  <!-- Favicons -->
-  <link href="images/newfavicon.png" rel="icon">
-  <link href="img/apple-touch-icon.png" rel="apple-touch-icon">
-
-  <!-- Google Fonts -->
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Poppins:300,400,500,700|Michroma|Montserrat" rel="stylesheet">
-  <!-- Bootstrap CSS File -->
-  <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-  <!-- Libraries CSS Files -->
-  <link href="lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
-  <link href="lib/animate/animate.min.css" rel="stylesheet">
-
-  <!-- Main Stylesheet File -->
-  <link href="css/style.css" rel="stylesheet">
-
-  <!-- =======================================================
-    Theme Name: Regna
-    Theme URL: https://bootstrapmade.com/regna-bootstrap-onepage-template/
-    Author: BootstrapMade.com
-    License: https://bootstrapmade.com/license/
-  ======================================================= -->
-</head>
-
-<body>
-
-  <!--==========================
-  Header
-  ============================-->
-
-  <!--==========================
-  Header
-  ============================-->
-  <header id="header">
-    <div class="container">
-
-      <div id="logo" class="pull-left">
-      <a href="#home" class="no-hover-highlight">
-        <!-- <img src="images/newfavicon.png" class="" style="margin-top:-16px; height: 50px" alt="" title="" /> -->
-        <span style="font-family: 'Montserrat'; letter-spacing:1px; font-size: 2em; font-style: regular">&nbsp;Tatham Inc</span>
-        </a>
-        <!-- Uncomment below if you prefer to use a text logo -->
-        <!--<h1><a href="#home">Regna</a></h1>-->
-      </div>
-
-    </div>
-  </header><!-- #header -->
-
-  <main id="main">
+@section('content')
     <!--==========================
       Case Study Section
     ============================-->
@@ -97,46 +42,4 @@
       </div>
     </section><!-- #facts -->
   </main>
-  <!--==========================
-    Footer
-  ============================-->
-  <!--==========================
-    Footer
-  ============================-->
-  <footer id="footer">
-    <div class="footer-top">
-      <div class="container">
-
-      </div>
-    </div>
-
-    <div class="container">
-      <div class="copyright">
-        &copy; Copyright <strong>Tatham Inc</strong>. All Rights Reserved
-      </div>
-
-    </div>
-  </footer><!-- #footer -->
-
-  <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
-
-  <!-- JavaScript Libraries -->
-  <script charset="ISO-8859-1" src="https://fast.wistia.com/assets/external/E-v1.js" async></script>
-  <script src="lib/jquery/jquery.min.js"></script>
-  <script src="lib/jquery/jquery-migrate.min.js"></script>
-  <script src="lib/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="lib/easing/easing.min.js"></script>
-  <script src="lib/wow/wow.min.js"></script>
-  <script src="lib/waypoints/waypoints.min.js"></script>
-  <script src="lib/counterup/counterup.min.js"></script>
-  <script src="lib/superfish/hoverIntent.js"></script>
-  <script src="lib/superfish/superfish.min.js"></script>
-
-  <!-- Contact Form JavaScript File -->
-  <script src="contactform/contactform.js"></script>
-
-  <!-- Template Main Javascript File -->
-  <script src="js/main.js"></script>
-  @stack('inline-scripts')
-
-</body>
+@endsection
