0acd9ffaa9e3d5000f739dc9c171f535192a1dae
Jeremy Ravet <gamemaker6.1@gmail.com>
2020-03-24 15:38:25 -0700

    Search bar change

--- resources/views/posts/_search_form.blade.php
+++ resources/views/posts/_search_form.blade.php
@@ -1,9 +1,11 @@
-{!! Form::open(['route' => 'blog', 'class' => 'd-flex', 'method' => 'GET']) !!}
-  <div class="input-group mr-sm-3">
-    {!! Form::text('q', null, ['class' => 'form-control', 'placeholder' => __('posts.search')]) !!}
-  </div>
+<div class="mt-3 d-flex justify-content-center">
+    {!! Form::open(['route' => 'blog', 'class' => 'd-flex', 'method' => 'GET']) !!}
+      <div class="input-group mr-sm-3">
+        {!! Form::text('q', null, ['class' => 'form-control', 'placeholder' => __('posts.search')]) !!}
+      </div>
 
-  <button type="submit" class="btn btn-primary">
-    <i class="fa fa-search" aria-hidden="true"></i>
-  </button>
-{!! Form::close() !!}
+      <button type="submit" class="btn btn-primary">
+        <i class="fa fa-search" aria-hidden="true"></i>
+      </button>
+    {!! Form::close() !!}
+</div>
