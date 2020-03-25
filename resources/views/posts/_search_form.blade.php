daea617c0303d1d447b594a4ad55b9f3bf69da74
Jeremy Ravet <gamemaker6.1@gmail.com>
2020-03-25 12:42:41 -0700

    Widened search bar

--- resources/views/posts/_search_form.blade.php
+++ resources/views/posts/_search_form.blade.php
@@ -1,11 +1,13 @@
 <div class="mt-3 d-flex justify-content-center">
-    {!! Form::open(['route' => 'blog', 'class' => 'd-flex', 'method' => 'GET']) !!}
-      <div class="input-group mr-sm-3">
-        {!! Form::text('q', null, ['class' => 'form-control', 'placeholder' => __('posts.search')]) !!}
-      </div>
+    <div class="w-100" style="max-width: 600px;">
+        {!! Form::open(['route' => 'blog', 'class' => 'd-flex', 'method' => 'GET']) !!}
+          <div class="input-group mr-sm-3">
+            {!! Form::text('q', null, ['class' => 'form-control', 'placeholder' => __('posts.search')]) !!}
+          </div>
 
-      <button type="submit" class="btn btn-primary">
-        <i class="fa fa-search" aria-hidden="true"></i>
-      </button>
-    {!! Form::close() !!}
+          <button type="submit" class="btn btn-primary">
+            <i class="fa fa-search" aria-hidden="true"></i>
+          </button>
+        {!! Form::close() !!}
+    </div>
 </div>
